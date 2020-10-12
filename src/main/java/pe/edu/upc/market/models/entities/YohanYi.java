97a41f122efc4e4f010f6ddcbb94ba19e843f1fd
Yohan Yi <f.yc1998@gmail.com>
2020-10-12 15:35:35 -0500

    Added YohanYi Entity

---
+++ src/main/java/pe/edu/upc/market/models/entities/YohanYi.java
@@ -0,0 +1,30 @@
+package pe.edu.upc.market.models.entities;
+
+
+
+
+public class YohanYi {	
+	
+	private Float id;
+	private String nombre;
+	private int edad;
+	
+	public Float getId() {
+		return id;
+	}
+	public void setId(Float id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+}
