bc3fc62d1684d5a9f880cd0466793945e3692d2b
unknown <roro.200599233@gmail.com>
2020-10-12 15:44:12 -0500

    Added RodrigoReaño Class

---
+++ src/main/java/pe/edu/upc/market/models/entities/RodrigoReaño.java
@@ -0,0 +1,25 @@
+package pe.edu.upc.market.models.entities;
+
+public class RodrigoReaño {
+	private Float id;
+	private Integer edad;
+	private String nombres;
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public Float getId() {
+		return id;
+	}
+	public void setId(Float id) {
+		this.id = id;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+}
