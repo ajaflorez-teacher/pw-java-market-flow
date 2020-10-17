779c9b08db9ace7f6316469cb7cb0fc1d21ed3c7
PieroCarrion <piero.january15@gmail.com>
2020-10-17 18:41:42 -0500

    Added PieroCarrion Class

---
+++ src/main/java/pe/edu/upc/market/models/entities/PieroCarrion17.java
@@ -0,0 +1,26 @@
+package pe.edu.upc.market.models.entities;
+
+public class PieroCarrion17 {
+	private Float id;
+	private String cadena;
+	private PieroCarrion17 pierocarrion;
+	public Float getId() {
+		return id;
+	}
+	public void setId(Float id) {
+		this.id = id;
+	}
+	public String getCadena() {
+		return cadena;
+	}
+	public void setCadena(String cadena) {
+		this.cadena = cadena;
+	}
+	public PieroCarrion17 getPierocarrion() {
+		return pierocarrion;
+	}
+	public void setPierocarrion(PieroCarrion17 pierocarrion) {
+		this.pierocarrion = pierocarrion;
+	}
+	
+}
