1f328bfd6f28440ffd8fdfe2c2b1da5a17a26904
Guillermo Gonzales Cáceres <guillerfut@hotmail.com>
2020-10-17 17:06:32 -0500

    Added GuillermoGonzales17 Class

---
+++ src/main/java/pe/edu/upc/market/models/entities/GuillermoGonzales17.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.market.models.entities;
+
+public class GuillermoGonzales17 {
+	private Float id;
+	private String cadena;
+	private GuillermoGonzales guillermoGonzales;
+	public Float getId() {
+		return id;
+	}
+	public void setId(Float id) {
+		this.id = id;
+	}
+	public String getCadena() {
+		return cadena;
+	}
+	public void setCadena(String cadena) {
+		this.cadena = cadena;
+	}
+	public GuillermoGonzales getGuillermoGonzales() {
+		return guillermoGonzales;
+	}
+	public void setGuillermoGonzales(GuillermoGonzales guillermoGonzales) {
+		this.guillermoGonzales = guillermoGonzales;
+	}
+	
+
+}
