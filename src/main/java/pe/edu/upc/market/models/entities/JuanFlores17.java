392fc0fec82fc1484344b3100aac941b4a13e795
Juan Flores Moroco <ajaflorez.upc@gmail.com>
2020-10-17 17:24:51 -0500

    Added fecha attribute in JuanFlores17 Class

--- src/main/java/pe/edu/upc/market/models/entities/JuanFlores17.java
+++ src/main/java/pe/edu/upc/market/models/entities/JuanFlores17.java
@@ -1,9 +1,13 @@
 package pe.edu.upc.market.models.entities;
 
+import java.util.Date;
+
 public class JuanFlores17 {
 	private Float id;
 	private String cadena;
 	private JuanFlores juanFlores;
+	private Date fecha;
+	
 	public Float getId() {
 		return id;
 	}
@@ -21,6 +25,12 @@ public class JuanFlores17 {
 	}
 	public void setJuanFlores(JuanFlores juanFlores) {
 		this.juanFlores = juanFlores;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
 	}
 	
 	
