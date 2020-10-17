30ec1b6f43f11c790d3bc1bf6e7d988c309b09d6
Jason Martinez Sanchez <jason_alex_13@hotmail.com>
2020-10-17 15:05:24 -0500

    Added jasonmartinezs class

---
+++ src/main/java/pe/edu/upc/market/models/entities/jasonmartinezs.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.market.models.entities;
+
+public class jasonmartinezs {
+
+	private String nombre;
+	private String carrera;
+	private String direccion;
+	private Integer edad;
+}
