5b9f0ac1d188d314c0e95769801614177521003e
Diego-Pz <diegopaz25814@gmail.com>
2020-10-17 17:26:50 -0500

    Added attribute in DiegoPaz17 Class

--- src/main/java/pe/edu/upc/market/models/entities/DiegoPaz17.java
+++ src/main/java/pe/edu/upc/market/models/entities/DiegoPaz17.java
@@ -1,5 +1,32 @@
 package pe.edu.upc.market.models.entities;
 
 public class DiegoPaz17 {
+private Float id;
+private String cadena;
+private DiegoPaz17 DiegoPaz17;
+
+public String getCadena() {
+	return cadena;
+}
+
+public void setCadena(String cadena) {
+	this.cadena = cadena;
+}
+
+public DiegoPaz17 getDiegoPaz17() {
+	return DiegoPaz17;
+}
+
+public void setDiegoPaz17(DiegoPaz17 diegoPaz17) {
+	DiegoPaz17 = diegoPaz17;
+}
+
+public Float getId() {
+	return id;
+}
+
+public void setId(Float id) {
+	this.id = id;
+}
 
 }
