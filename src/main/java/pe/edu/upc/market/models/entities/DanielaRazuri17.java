8b85e77798a89fa8ada31901f50984d3f8154daa
Daniela Razuri-Requis <razdani800@gmail.com>
2020-10-25 14:14:57 -0500

    Added DanielaRazuri17

---
+++ src/main/java/pe/edu/upc/market/models/entities/DanielaRazuri17.java
@@ -0,0 +1,29 @@
+package pe.edu.upc.market.models.entities;
+
+public class DanielaRazuri17 {
+
+	private Float id;
+	private String cadena;
+	private DanielaRazuri danielaRazuri;
+	public Float getId() {
+		return id;
+	}
+	public void setId(Float id) {
+		this.id = id;
+	}
+	public String getCadena() {
+		return cadena;
+	}
+	public void setCadena(String cadena) {
+		this.cadena = cadena;
+	}
+	public DanielaRazuri getDanielaRazuri() {
+		return danielaRazuri;
+	}
+	public void setDanielaRazuri(DanielaRazuri danielaRazuri) {
+		this.danielaRazuri = danielaRazuri;
+	}
+	
+	
+	
+}
